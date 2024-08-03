3095c2331adcd0daf7e6c9814794e9283731f603
Arman <arman.41@hotmail.com>
2024-08-03 15:24:19 +0700

    latest updates

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,19 +11,18 @@ function Nav() {
           <p className="title">Fatia Marwa Nastitie</p>
           <p className="desc">Data Analyst</p>
           <p className="desc-new">
-            Hi, I'm Fatia. Experienced Data Analyst with a demonstrated history{' '}
-            of working in Telco Industry{' '}
-            and Financial Technology. Currently, I am a Master of Science in{' '}
-            Applied Data Science Candidate in University of Southern California.{' '}
-            Prior to that, I graduated with a Bachelor's Degree in Statistics{' '}
-            from University of Indonesia with Cum Laude Honor.
+            Hi, I’m Fatia. Experienced Data Analyst with 2.5 years of expertise in building ML models and frameworks 
+            within the Telco industry, and an additional year in the E-Commerce sector. 
+            Currently, I am pursuing an MS in Applied Data Science at the University of Southern California, 
+            Viterbi School of Engineering. I hold a Bachelor’s Degree in Statistics from the University of Indonesia, 
+            graduating with Cum Laude honors.
           </p>
         </div>
       </Link>
       <div className="nav-links">
         <NavLink exact to="/" activeClassName="active">Portfolio</NavLink>
         <NavLink exact to="/contact" activeClassName="active">Find me on</NavLink>
-        <a href="https://drive.google.com/file/d/1nVxjA8HENd4rpST9rlQvJJGoeTJ2duer/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a>
+        <a href="https://drive.google.com/file/d/1WLEDtqdF7CCGPmJCkZPdA5SZqf5t0XR4/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a>
       </div>
     </div>
   );
